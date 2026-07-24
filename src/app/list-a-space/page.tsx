3015f61604ae75cde9ab9242a31f74f62bbe74ab
Jamie <jamie@blanked.melbourne>
2026-07-24 21:17:47 +0100

    Align List Your Space hero margin with Browse Spaces page

--- src/app/list-a-space/page.tsx
+++ src/app/list-a-space/page.tsx
@@ -133,7 +133,7 @@ export default function ListASpacePage() {
   return (
     <>
       <div className="flex min-h-screen flex-col lg:flex-row">
-        <div className="flex flex-col justify-center gap-6 px-6 py-16 sm:px-10 lg:w-1/2 lg:px-14 lg:py-0">
+        <div className="flex flex-col justify-center gap-6 px-1.5 py-16 lg:w-1/2 lg:py-0">
           <h1 className="text-6xl font-bold uppercase leading-[0.95] tracking-tight sm:text-7xl lg:text-[100px]">
             List your
             <br />
